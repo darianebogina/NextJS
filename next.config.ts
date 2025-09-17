75bd02996eb7e9ae0e3d7692dbcd5ce2f653d49e
Daria <dasha.nebogina@mail.ru>
2025-09-17 09:18:33 +0300

    feat: implement upload images with different settings

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,24 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img3.akspic.ru',
+        port: '',
+        pathname: '/previews/**',
+        search: ''
+      },
+      {
+        protocol: 'https',
+        hostname: 'img2.akspic.ru',
+        port: '',
+        pathname: '/previews/**',
+        search: ''
+      },
+    ],
+  },
 };
 
 export default nextConfig;
